db828aaabc0a9192cf863cd1bd6438a41eaec273
Ehsaan Hoque <ehsaanhoque57@gmail.com>
2025-08-19 23:05:41 +0100

    setup of research and writer crew

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from csv import writer
 import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
@@ -7,7 +8,7 @@ load_dotenv()
 # Initialise the search tool. Agents will use this to browse the internet
 search_tool = SerperDevTool()
 
-# --- Define Your Agents ---
+# --- Define the agents ---
 
 # Agent 1: The Researcher
 researcher = Agent(
@@ -20,3 +21,48 @@ researcher = Agent(
     alow_delegation=False,
     tools=[search_tool]
 )
+
+# Agent 2: The Writer
+writer = Agent(
+    role='Expert Tech Content Strategist',
+    goal='Craft compelling and informative reports from research findigs.',
+    backstory="""You are a content strategist, famous for your ability to transform complex
+    research into clear and insightful reports. You know how to structure a narrative that is both 
+    informative and easy to read.""",
+    verbose=True,
+    allow_delegation=True
+)
+
+# --- Define the tasks ---
+
+# Task 1: The Research Task
+research_task = Task(
+    description="""Conduct an in-depth analysis of the latest trends in {topic}. Identify
+    key players, innovations, and market dynamics. Your final answer MUST be a full analysis report.""",
+    expected_output='A comprehensive, data-driven report on the latest AI trends.',
+    agent=researcher
+)
+
+# Task 2: The Writing Task
+write_task = Task (
+    description="""Using the research findings, write an engaging report on {topic}.
+    The report should be well-structured with a clear introduction, body, and conclusion. It should highlight
+    the most important trends and insights.""",
+    expected_output='A polished and insightful report on {topic}, formatted as markdown.',
+    agent=writer
+)
+
+# --- Assemble the Crew ---
+# Create the Crew, specify agents, tasks and process
+research_crew = Crew(
+    agents=[researcher, writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential,
+    verbose=True
+)
+
+topic_to_research = 'The Future of AI in Sports'
+result = research_crew.kickoff(inputs={'topic': topic_to_research})
+
+print("--- Here is the result ---")
+print("\n", result)
